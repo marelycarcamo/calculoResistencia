258cbf979f3ec8c488fe1ac022b70f2c58457be0
marelycarcamo <44790921+marelycarcamo@users.noreply.github.com>
2023-04-06 03:44:45 -0400

    validaçión numeros positivos

--- src/resistencia/Resistencia.java
+++ src/resistencia/Resistencia.java
@@ -1,3 +1,6 @@
+/*
+ * ESTE PROGRAMA CALCULA LA SUMA TOTAL DE RESISTENCIA
+ */
 package resistencia;
 
 import java.util.Scanner;
@@ -9,57 +12,78 @@ public class Resistencia {
 		Scanner scanner = new Scanner(System.in);
 
 		// declaración e inicialización de variables
+
 		double r1 = 0;
 		double r2 = 0;
 		double r3 = 0;
 		boolean bError = true;
+
+		// validación primera resistencia
+
 		do {
 
 			try {
 				System.out.println("Introduce la primera resistencia: ");
 				r1 = scanner.nextDouble();
 				bError = false;
-
+				if (r1 <= 0) {
+					System.out.println("ERROR. Ingresar un número positivo\n ");
+					bError = true;
+				}
 			} catch (InputMismatchException e) {
-				System.out.print("Error, debe ingresar un valor numérico");
+				System.out.println("Error, debe ingresar un valor numérico\n ");
 				bError = true;
-				scanner.nextLine();
+				scanner.nextLine(); // limpia buffer
 			}
 
 		} while (bError);
 
+		// validación segunda resistencia
+
 		do {
 
 			try {
-				System.out.println("Introduce la primera resistencia: ");
+				System.out.println("Introduce la segunda resistencia: ");
 				r2 = scanner.nextDouble();
 				bError = false;
+				if (r2 <= 0) {
+					System.out.println("ERROR. Ingresar un número positivo\n ");
+					bError = true;
+				}
 
 			} catch (InputMismatchException e) {
-				System.out.print("Error, debe ingresar un valor numérico");
+				System.out.print("ERROR. Ingresar un valor numérico\n");
 				bError = true;
 				scanner.nextLine();
 			}
 
 		} while (bError);
+
+		// validación tercera resistencia
 
 		do {
 
 			try {
-				System.out.println("Introduce la primera resistencia: ");
+				System.out.println("Introduce la tercera resistencia: ");
 				r3 = scanner.nextDouble();
 				bError = false;
-
+				if (r3 <= 0) {
+					System.out.println("ERROR. Ingresar un número positivo\n ");
+					bError = true;
+				}
 			} catch (InputMismatchException e) {
-				System.out.print("Error, debe ingresar un valor numérico");
+				System.out.print("Error, debe ingresar un valor numérico \n");
 				bError = true;
 				scanner.nextLine();
 			}
 
 		} while (bError);
 
-//	 calculo del total de resistencia
+		// calculo del total de resistencia
+
 		double rt = 1 / (1 / r1 + 1 / r2 + 1 / r3);
+
+		// despliegue de resultado
 
 		System.out.println("La resistencia total es: " + rt);
 
